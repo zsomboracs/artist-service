16f57c8d36012a0be32b179859e3f505ca900ad4
Zsombor Acs <v-zsacs@expedia.com>
2017-11-23 14:57:15 +0100

    service skeleton from spring io

---
+++ src/main/java/com/epam/experiment/artistservice/ArtistServiceApplication.java
@@ -0,0 +1,12 @@
+package com.epam.experiment.artistservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtistServiceApplication.class, args);
+	}
+}
